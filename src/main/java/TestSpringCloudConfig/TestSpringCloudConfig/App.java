ae7983040bf1a2915aec095339826529d7eb3d4c
lili <lili@ESG.360ES.CN>
2019-05-30 13:48:54 +0800

    first commit

---
+++ src/main/java/TestSpringCloudConfig/TestSpringCloudConfig/App.java
@@ -0,0 +1,13 @@
+package TestSpringCloudConfig.TestSpringCloudConfig;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
